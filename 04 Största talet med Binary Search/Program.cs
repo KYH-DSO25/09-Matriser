17428fec67583d79036b71f00fc5c4d39bbb0c2c
Claes Engelin <claes.engelin@zocom.se>
2025-10-08 11:39:04 +0200

    Efter uppgift 4

--- 04 Största talet med Binary Search/Program.cs
+++ 04 Största talet med Binary Search/Program.cs
@@ -6,12 +6,43 @@
 
 
 
+Console.Write("Ange storleken på vektorn (N): ");
+int n = int.Parse(Console.ReadLine());
 
+Console.Write("Ange ett tal K: ");
+int k = int.Parse(Console.ReadLine());
 
+int[] numbers = new int[n];
+Console.WriteLine("\nAnge {0} tal att lägga in i vektorn: ", n );
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write("    {0}: ", i + 1);
+    numbers[i] = int.Parse(Console.ReadLine());
+}
 
+PrintLargestNumber(numbers, k);
 
-
-
-
-Console.Write("Tryck på en tangent för att stänga fönstret...");
+Console.Write("\n\nTryck på en tangent för att stänga fönstret...");
 Console.ReadKey();
+
+
+// Skriver ut största talet som är <= k med hjälp av metoden Array.BinarySearch()
+void PrintLargestNumber(int[] numbers, int k)
+{
+    Array.Sort(numbers);
+
+    int index = Array.BinarySearch(numbers, k);
+    index = index >= 0 ? index : (index == -1 ? -1 : Math.Abs(index + 2));
+
+    if (index != -1)
+    {
+        Console.WriteLine("\nHittade tal <= K: {0}", k);
+        Console.WriteLine("  -> Resultat: {0}\n", numbers[index]);
+    }
+    else
+    {
+        Console.WriteLine("\n - Det finns inga tal <= K, {0}, i vektorn", k);
+        Console.WriteLine("index = {0}", index);
+    }
+}
+
